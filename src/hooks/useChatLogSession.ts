6101d60fddb3f1d2f647eb37340ffd41b7d05d5b
Sounho Chung <aschung01@snu.ac.kr>
2023-11-17 23:59:18 +0900

    FIX : Fixed malformed argument bug

--- src/hooks/useChatLogSession.ts
+++ src/hooks/useChatLogSession.ts
@@ -8,7 +8,10 @@ export const useRunLog = (versionUuid: string) => {
   const { data: chatLogSessionListData } = useQuery({
     queryKey: ["chatLogSessionListData", { versionUuid: versionUuid }],
     queryFn: async () =>
-      await fetchChatLogSessions(await createSupabaseClient(), versionUuid),
+      await fetchChatLogSessions({
+        supabaseClient: await createSupabaseClient(),
+        versionUuid: versionUuid,
+      }),
     enabled: versionUuid != undefined && versionUuid != null,
   });
 
