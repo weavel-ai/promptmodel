7861603680b8cf6a57e02e5cb01323307b412f16
Sounho Chung <aschung01@snu.ac.kr>
2024-01-09 23:04:36 +0900

    fix: amend alembic migration history

--- backend/migrations/versions/f4cbb53594f3_add_projectdailyrunlogmetric_view.py
+++ backend/migrations/versions/f4cbb53594f3_add_projectdailyrunlogmetric_view.py
@@ -1,7 +1,7 @@
 """ADD projectDailyRunLogMetric view
 
 Revision ID: f4cbb53594f3
-Revises: 73f8df3ae3ae
+Revises: 97ad559a7d01
 Create Date: 2024-01-08 16:58:24.268308
 
 """
@@ -13,7 +13,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = "f4cbb53594f3"
-down_revision: Union[str, None] = "73f8df3ae3ae"
+down_revision: Union[str, None] = "97ad559a7d01"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
