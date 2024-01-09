36b5978a48dc6b803356638a25dcc5b62dde0c30
engineerA314 <engineerA314@github.com>
2024-01-09 11:52:04 +0900

    fix(listener): add periodic ping check

--- pg_to_redis_listener/listener.py
+++ pg_to_redis_listener/listener.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from typing import Optional
 import time
 import os
 import json
@@ -6,10 +7,12 @@ import select
 import sys
 import psycopg2
 import psycopg2.extensions
+from psycopg2.extensions import connection as pg_connection
 import redis
+from redis import Redis
 
 
-def connect_to_postgres():
+def connect_to_postgres() -> Optional[pg_connection]:
     print("Start connecting to PostgreSQL")
     try:
         conn = psycopg2.connect(
@@ -26,7 +29,7 @@ def connect_to_postgres():
         return None
 
 
-def connect_to_redis():
+def connect_to_redis() -> Optional[Redis]:
     print("Start connecting to Redis")
     try:
         redis_host = os.environ.get("REDIS_HOST") or "localhost"
@@ -40,17 +43,31 @@ def connect_to_redis():
         return None
 
 
-def listen_to_pg_channels(pg_conn, channels):
+def listen_to_pg_channels(pg_conn: Optional[pg_connection], channels: list):
     print("Starting to listen to PostgreSQL channels")
     cursors = {channel: pg_conn.cursor() for channel in channels}
     for channel, cursor in cursors.items():
         cursor.execute(f"LISTEN {channel}")
         print(f"Listening to {channel}")
+        
+def check_pg_connection(pg_conn: Optional[pg_connection]):
+    try:
+        with pg_conn.cursor() as cursor:
+            cursor.execute("SELECT 1")
+        return True
+    except psycopg2.OperationalError:
+        return False
+
+def check_redis_connection(redis_conn: Optional[Redis]):
+    try:
+        return redis_conn.ping()
+    except redis.RedisError:
+        return False
 
 
-pg_conn = connect_to_postgres()
+pg_conn: Optional[pg_connection] = connect_to_postgres()
 print("End connecting to PostgreSQL")
-redis_conn = connect_to_redis()
+redis_conn: Optional[Redis] = connect_to_redis()
 print("End connecting to Redis")
 
 if not pg_conn or not redis_conn:
@@ -69,30 +86,38 @@ channels = [
 
 listen_to_pg_channels(pg_conn, channels)
 
+last_check_time = time.time()
+
 while True:
+    current_time = time.time()
     try:
+        if current_time - last_check_time >= 3600:
+            print(f"Checking database connections at {datetime.now()}")
+            last_check_time = current_time
+            if not check_pg_connection(pg_conn):
+                pg_conn = connect_to_postgres()
+                if pg_conn:
+                    listen_to_pg_channels(pg_conn, channels)
+                else:
+                    print("Failed to reestablish PostgreSQL connection. Exiting...")
+                    raise psycopg2.DatabaseError
+            if not check_redis_connection(redis_conn):
+                redis_conn = connect_to_redis()
+                if not redis_conn:
+                    print("Failed to reestablish Redis connection. Exiting...")
+                    raise redis.RedisError
+        
         if select.select([pg_conn], [], [], 60) == ([], [], []):
             print(
                 f"Timeout ({datetime.now()}): No notifications in the last 60 seconds"
             )
         else:
             pg_conn.poll()
-            # while pg_conn.notifies:
-            #     notify = pg_conn.notifies.pop(0)
-            #     print("Got NOTIFY:", notify.channel)
-            #     if notify.payload:
-            #         data = json.loads(notify.payload)
-            #         channel = notify.channel
-            #         if data.get("project_uuid"):
-            #             channel += f"_p_{data['project_uuid']}"
-            #         if data.get("organization_id"):
-            #             channel += f"_o_{data['organization_id']}"
-            #         print(f"Publishing to {channel}")
-            #         redis_conn.publish(channel, notify.payload)
+
             for notify in pg_conn.notifies:
                 print("Got NOTIFY:", notify.channel)
                 if notify.payload:
-                    data = json.loads(notify.payload)
+                    data: dict = json.loads(notify.payload)
                     channel = notify.channel
                     if data.get("project_uuid"):
                         channel += f"_p_{data['project_uuid']}"
